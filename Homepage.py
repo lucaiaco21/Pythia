034506436c8898d98e92b4c59263cea98cdb2eec
Yassuhiro M <yassuhiro.m@gmail.com>
2026-02-07 20:11:57 +0100

    Fix column layout for student names display

--- Homepage.py
+++ Homepage.py
@@ -91,7 +91,7 @@ with st.container(border=False):
     st.markdown('<div class="group-header">GRUPO 6</div>', unsafe_allow_html=True)
     
     # Grid layout for students (2 columns of 3 names) for better spacing
-    s_col1, s_col2= st.columns(3)
+    s_col1, s_col2= st.columns(2)
     
     students = [
         "Anabel Jose Baéz Rodríguez",  "Ilan Alexander Arvelo Yagua",
