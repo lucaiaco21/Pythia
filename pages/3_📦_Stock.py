582eb5e497570e93b2a5c926f5619c9738c7623f
lucaiaco21 <luca.iacomino20@gmail.com>
2026-02-09 14:34:45 +0100

    Update 3_📦_Stock.py

--- pages/3_📦_Stock.py
+++ pages/3_📦_Stock.py
@@ -1,3 +1,21 @@
 import streamlit as st
-
+import pandas as pd
 st.title("_📊_Stock Page")
+
+
+@st.cache_data
+def load_data(file):
+    data = pd.read_csv(file)
+    return data
+
+with st.sidebar:
+    uploaded_file = st.file_uploader("Choose a file")
+
+    if uploaded_file is None:
+        st.info("Upload a file", icon='i')
+        st.stop()
+
+
+df = load_data(uploaded_file)
+with st.expander("Data Preview"):
+    st.dataframe(df)
