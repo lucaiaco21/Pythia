b9798fd5248e9bb7da07bec617dc8fa6d4e494b6
lucaiaco21 <luca.iacomino20@gmail.com>
2026-02-11 15:28:42 +0100

    Fix page title typo in Reviews page

--- pages/2_📦_Reviews.py
+++ pages/2_📦_Reviews.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 # Page configuration
 st.set_page_config(
-    page_title="Review analysis",
+    page_title="Reviews analysis",
     page_icon="☕",
     layout="wide",
     initial_sidebar_state="expanded"
